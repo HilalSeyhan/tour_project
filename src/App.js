930cfa7e15ba3e8c1bf5c183349b4b906255d2fc
HilalSeyhann <hilalseyhann@gmail.com>
2022-07-20 16:51:50 +0300

    data and design integration

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
 
+const url = 'http://localhost:3004/tourdata'
 
 function App() {
   const [loading, setLoading] = useState(true)
